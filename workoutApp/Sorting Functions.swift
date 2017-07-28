f604def24d52db832ef0a3e6464eabfa0d26bc05
Alexander Kvamme <alexanderkvamme@Alexanders-MacBook-Air.local>
2017-07-28 14:54:41 +0200

    sorting of lifts when displaying previous WorkoutLog, but it also displayed lifts that were not performed. Adding hasBeenPerformed property to Lifts

---
+++ workoutApp/Sorting Functions.swift
@@ -0,0 +1,9 @@
+//
+//  Sorting Functions.swift
+//  workoutApp
+//
+//  Created by Alexander Kvamme on 27/07/2017.
+//  Copyright © 2017 Alexander Kvamme. All rights reserved.
+//
+
+import Foundation
