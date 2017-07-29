eb22dbe670e467ef9da3eac11244d6c9ce76f920
Alexander Kvamme <alexanderkvamme@Alexanders-MacBook-Air.local>
2017-07-29 13:38:16 +0200

    now only seeds the essentials

---
+++ workoutApp/ReusableComponents.swift
@@ -0,0 +1,9 @@
+//
+//  ReusableComponents.swift
+//  workoutApp
+//
+//  Created by Alexander Kvamme on 29/07/2017.
+//  Copyright © 2017 Alexander Kvamme. All rights reserved.
+//
+
+import Foundation
