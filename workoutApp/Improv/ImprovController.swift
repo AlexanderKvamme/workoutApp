036701affed47c11e1d1024103193de862909896
Alexander Kvamme <alexanderkvamme@gmail.com>
2025-04-21 16:11:22 +0200

    Navigate in

--- workoutApp/Improv/ImprovController.swift
+++ workoutApp/Improv/ImprovController.swift
@@ -34,7 +34,9 @@ class HoneycombViewController: SelectionViewController {
         super.viewWillAppear(animated)
         // Hide the navigation bar
         navigationController?.setNavigationBarHidden(true, animated: animated)
+        print("will see if custom")
         if let customTabBarController = self.tabBarController as? CustomTabBarController {
+            print("had custom")
             customTabBarController.hideSelectionIndicator(shouldAnimate: true)
             navigationController?.setNavigationBarHidden(true, animated: true)
         }
@@ -462,43 +464,3 @@ class HexagonView: UIView {
 // MARK: - HexagonViewDelegate
 extension HoneycombViewController: HexagonViewDelegate {}
 
-// MARK: - ImprovWorkoutController
-class ImprovWorkoutController: UIViewController {
-    private let muscleGroup: Muscle
-    
-    init(muscleGroup: Muscle) {
-        self.muscleGroup = muscleGroup
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .akLight
-        
-        // Set up the view with the selected muscle group
-        setupView()
-    }
-    
-    private func setupView() {
-        // Title label
-        let titleLabel = UILabel()
-        titleLabel.text = muscleGroup.name
-        titleLabel.font = AKFont.round(.bold, 24)
-        titleLabel.textColor = .white
-        titleLabel.textAlignment = .center
-        
-        view.addSubview(titleLabel)
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
-        ])
-        
-        // Add more UI components as needed for your workout view
-    }
-}
---
+++ workoutApp/Improv/ImprovWorkoutController.swift
@@ -0,0 +1,49 @@
+import UIKit
+import AKKIT
+
+
+// MARK: - ImprovWorkoutController
+class ImprovWorkoutController: UIViewController {
+    private let muscleGroup: Muscle
+    
+    init(muscleGroup: Muscle) {
+        self.muscleGroup = muscleGroup
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .akLight
+        
+        // Set up the view with the selected muscle group
+        setupView()
+        styleBackButton()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
+    private func setupView() {
+        // Title label
+        let titleLabel = UILabel()
+        titleLabel.text = muscleGroup.name
+        titleLabel.font = AKFont.round(.bold, 24)
+        titleLabel.textColor = .white
+        titleLabel.textAlignment = .center
+        
+        view.addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
+        
+        // Add more UI components as needed for your workout view
+    }
+}
